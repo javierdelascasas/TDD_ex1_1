4b8c0485b32de0abdea1b29b083087a1bb462beb
Javier De Las Casas <javier.delascasaschung@gmail.com>
2018-08-11 23:29:47 +0200

    Add exercise TDD 1.4

--- src/se/javierdlc/Calculator.java
+++ src/se/javierdlc/Calculator.java
@@ -2,9 +2,17 @@ package se.javierdlc;
 
 public class Calculator {
     public int add(int x, int y) {
-        if(x <= 0 || y <= 0){
+        if (x <= 0 || y <= 0) {
             return 0;
         }
         return x + y;
+    }
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public int divide(int x, int y) {
+        return x / y;
     }
 }
--- tests/se/javierdlc/CalculatorTest.java
+++ tests/se/javierdlc/CalculatorTest.java
@@ -9,7 +9,7 @@ public class CalculatorTest {
     private Calculator sut;
 
     @Before
-    public void setup(){
+    public void setup() {
         sut = new Calculator();
     }
 
@@ -20,14 +20,26 @@ public class CalculatorTest {
     }
 
     @Test
-    public void whenTheFirstAddParameterIsNegativeShouldReturnZero(){
+    public void whenTheFirstAddParameterIsNegativeShouldReturnZero() {
         int result = sut.add(-5, 3);
-        Assert.assertEquals("Should return zero when first parameter passed to the method is negative number.",result, 0);
+        Assert.assertEquals("Should return zero when first parameter passed to the method is negative number.", result, 0);
     }
 
     @Test
-    public void whenTheSecondAddParameterIsNegativeShouldReturnZero(){
+    public void whenTheSecondAddParameterIsNegativeShouldReturnZero() {
         int result = sut.add(3, -6);
-        Assert.assertEquals("Should return zero when second parameter passed to the method is negative number.",result, 0);
+        Assert.assertEquals("Should return zero when second parameter passed to the method is negative number.", result, 0);
+    }
+
+    @Test
+    public void whenThreeTimesFiveShouldReturnFifteen() {
+        int result = sut.multiply(3, 5);
+        Assert.assertEquals("Should return fifteen when passed 3 and 5.", result, 15);
+    }
+
+    @Test
+    public void whenFifteenDividedByFiveShouldReturnThree() {
+        int result = sut.divide(15, 5);
+        Assert.assertEquals("Should return three when passed 15 and 5", result, 3);
     }
 }
