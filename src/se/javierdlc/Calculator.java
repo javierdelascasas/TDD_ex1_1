8061c47dcdd12a596692705c6b442622e318633d
Javier De Las Casas <javier.delascasaschung@gmail.com>
2018-08-11 23:00:20 +0200

    Add exercise TDD 1.3

--- src/se/javierdlc/Calculator.java
+++ src/se/javierdlc/Calculator.java
@@ -2,7 +2,7 @@ package se.javierdlc;
 
 public class Calculator {
     public int add(int x, int y) {
-        if(x <= 0){
+        if(x <= 0 || y <= 0){
             return 0;
         }
         return x + y;
--- tests/se/javierdlc/CalculatorTest.java
+++ tests/se/javierdlc/CalculatorTest.java
@@ -1,21 +1,33 @@
 package se.javierdlc;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
 
+    private Calculator sut;
+
+    @Before
+    public void setup(){
+        sut = new Calculator();
+    }
+
     @Test
     public void whenAddingTwoPlusThreeShouldReturnFive() {
-        Calculator sut = new Calculator();
         int result = sut.add(2, 3);
         Assert.assertEquals(result, 5);
     }
 
     @Test
     public void whenTheFirstAddParameterIsNegativeShouldReturnZero(){
-        Calculator sut = new Calculator();
         int result = sut.add(-5, 3);
-        Assert.assertEquals("Should return zero when negative numbers is passed to the method.",result, 0);
+        Assert.assertEquals("Should return zero when first parameter passed to the method is negative number.",result, 0);
+    }
+
+    @Test
+    public void whenTheSecondAddParameterIsNegativeShouldReturnZero(){
+        int result = sut.add(3, -6);
+        Assert.assertEquals("Should return zero when second parameter passed to the method is negative number.",result, 0);
     }
 }
